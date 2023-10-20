b83c53e6909da2f00a677be7d8849a234293e6cf
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2023-10-19 21:40:25 -0400

    Add backend code.

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import Express from "express";
+import OpenAI from "openai";
+
+const express = new Express();
+const openai = new OpenAI();
+const REQUEST_PROMPT = "Summarize the health effects of these ingredients. Be critical. If there is even a slight impact please state it. Give me the ingredients to their descriptions as a json response. Separate the ingredients as they are separated by commas. Respond ONLY with an array that has JSON objects with the parameters ingredient and description.";
+
+express.get("/:upc", (req, res) => {
+    fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${req.params.upc}&pageSize=1&api_key=${process.env["FDC_API_KEY"]}`).then((fdc_res) => {
+        fdc_res.json().then(async (food_data) => {
+            const chatCompletion = await openai.chat.completions.create({
+                messages: [{role: 'user', content: `${food_data.foods[0].ingredients} ${REQUEST_PROMPT}`}],
+                model: 'gpt-3.5-turbo',
+            });
+
+            console.log(chatCompletion.choices);
+            res.send(chatCompletion.choices);
+        });
+
+    }).catch((err) => {
+        console.error(err);
+    });
+});
+
+express.listen(process.env["PORT"], () => {
+    console.log(`Qodi API is now listening on port ${process.env["PORT"]}.`);
+});
