ee8a0188a27aa6417726737b487fb995e9d49c4b
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2023-10-21 22:19:18 -0400

    Update logging

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,17 @@ const openai = new OpenAI();
 const REQUEST_PROMPT = "Summarize the health effects of these ingredients. Be critical. If there is even a slight impact please state it. Separate the ingredients as they are separated by commas. Respond ONLY with an array that has JSON objects with the parameters ingredient and description.";
 
 express.get("/:upc", (req, res) => {
+    console.log(`Received request for UPC ${req.params.upc}.`)
     fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${req.params.upc}&pageSize=1&api_key=${process.env["FDC_API_KEY"]}`).then((fdc_res) => {
         fdc_res.json().then(async (food_data) => {
             if (food_data.foods[0]) {
+                console.log(`Found food with UPC ${req.params.upc}.`);
                 const chatCompletion = await openai.chat.completions.create({
                     messages: [{role: "user", content: `${food_data.foods[0].ingredients} ${REQUEST_PROMPT}`}],
                     model: "gpt-3.5-turbo",
                 });
 
-                console.log(chatCompletion.choices);
+                console.log(`Responding with ${chatCompletion.choices[0].message.content}.`);
                 res.send(chatCompletion.choices[0].message.content);
             }
         });
