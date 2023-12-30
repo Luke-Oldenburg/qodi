412eb93001827dc4ae73bea20d7a2ba1e7b2c9a6
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-12-29 20:51:29 -0500

    fix no ai ingredients edge case

--- next-app/src/app/api/[upc]/route.ts
+++ next-app/src/app/api/[upc]/route.ts
@@ -11,7 +11,7 @@ const prisma = new PrismaClient().$extends(withAccelerate());
 
 // Define GPT-3.5 prompt for requesting health effects of ingredients
 const REQUEST_PROMPT =
-  "Summarize the health effects of these ingredients. Be critical. If there is even a slight impact please state it. If there are ingredients labeled as \"may contain\" or \"less than 2% of\" please still include them and indicate it in your response. Separate the ingredients as they are separated by commas. Respond ONLY with an array that has JSON objects with the parameters ingredient and description.";
+  'Summarize the health effects of these ingredients. Be critical. If there is even a slight impact please state it. If there are ingredients labeled as "may contain" or "less than 2% of" please still include them and indicate it in your response. Separate the ingredients as they are separated by commas. Respond ONLY with an array that has JSON objects with the parameters ingredient and description.';
 
 export const runtime = "edge";
 
@@ -46,64 +46,67 @@ export async function GET(
 
     // Check if ingredients are already in database
     for (let ingredient of food_data.foods[0].ingredients.split(", ")) {
-      const ingredientExists: { description: string } | null = await prisma.ingredients.findUnique({
-        where: {
-          ingredient: ingredient,
-        }
-      });
+      const ingredientExists: { description: string } | null =
+        await prisma.ingredients.findUnique({
+          where: {
+            ingredient: ingredient,
+          },
+        });
 
       if (ingredientExists) {
-        response.push({ingredient: ingredient,
-          description: ingredientExists.description});
-        
+        response.push({
+          ingredient: ingredient,
+          description: ingredientExists.description,
+        });
       } else {
         aiIngredients += ingredient + ", ";
       }
     }
 
-    // Construct prompt and send to OpenAI API
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [
-        {
-          role: "user",
-          content: `Ingredients: ${aiIngredients}\n${REQUEST_PROMPT}`,
-        },
-      ],
-      model: "gpt-3.5-turbo",
-    });
-
-    // Parse JSON response from OpenAI API
-    let content = chatCompletion.choices[0].message.content;
-    console.log(content);
-
-    // Check if chatGPT returned a response
-    if (!content) {
-      console.error("No response from OpenAI.");
-      return Response.error();
-    }
-
-    // Add ingredients from chatGPT to response and database
-    let contentObj = JSON.parse(content);
-    for (let ingredient of contentObj) {
-      response.push(ingredient);
-      await prisma.ingredients.upsert({
-        create: {
-          ingredient: ingredient.ingredient,
-          description: ingredient.description,
-        },
-        where: {
-          ingredient: ingredient.ingredient,
-        },
-        update: {
-          ingredient: ingredient.ingredient,
-          description: ingredient.description,
-        },
+    if (aiIngredients.length > 0) {
+      // Construct prompt and send to OpenAI API
+      const chatCompletion = await openai.chat.completions.create({
+        messages: [
+          {
+            role: "user",
+            content: `Ingredients: ${aiIngredients}\n${REQUEST_PROMPT}`,
+          },
+        ],
+        model: "gpt-3.5-turbo",
       });
+
+      // Parse JSON response from OpenAI API
+      let content = chatCompletion.choices[0].message.content;
+      console.log(content);
+
+      // Check if chatGPT returned a response
+      if (!content) {
+        console.error("No response from OpenAI.");
+        return Response.error();
+      }
+
+      // Add ingredients from chatGPT to response and database
+      let contentObj = JSON.parse(content);
+      for (let ingredient of contentObj) {
+        response.push(ingredient);
+        await prisma.ingredients.upsert({
+          create: {
+            ingredient: ingredient.ingredient,
+            description: ingredient.description,
+          },
+          where: {
+            ingredient: ingredient.ingredient,
+          },
+          update: {
+            ingredient: ingredient.ingredient,
+            description: ingredient.description,
+          },
+        });
+      }
     }
 
     // Send response back to the client
     return Response.json(response);
-
   } else {
     console.error(`No ingredients found for UPC ${upc}.`);
     return Response.error();
