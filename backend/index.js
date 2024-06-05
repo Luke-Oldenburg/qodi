e479e5d7ca004148fd9a6cb4cb9a6b2a09bf1315
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2024-06-05 18:31:01 -0400

    Add info endpoint and rename main endpoint to health

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,9 @@ const REQUEST_PROMPT = `Summarize the health effects of these ingredients. Be cr
   description should be a description of the health impacts of the ingredient and any potential risks associated with it.`;
 
 // Define Express route for GET requests with a UPC code in the path
-express.get("/:upc", async (req, res) => {
+express.get("/info/:upc", async (req, res) => {
   const upc = req.params.upc;
-  console.log(`Received request for UPC ${upc}`);
+  console.log(`Received GET request for /name/${upc}`);
 
   // Fetch food data from FDC (FoodData Central) API
   let fdc_res = null;
@@ -36,7 +36,41 @@ express.get("/:upc", async (req, res) => {
   // Parse JSON response from FDC API
   const food_data = await fdc_res.json();
 
-  // Make sure that ingredients were found
+  // Make sure that the food item was found
+  if (food_data.foods[0]) {
+    let response = {
+      "name": food_data.foods[0].description,
+      "brand": food_data.foods[0].brandName
+    };
+    console.log("Found name and brand:");
+    console.table(response);
+    res.json(response);
+
+  } else {
+    console.error(`No food item found for UPC ${upc}.`);
+    return Response.error();
+  }
+});
+
+express.get("/health/:upc", async (req, res) => {
+  const upc = req.params.upc;
+  console.log(`Received GET request for /info/${upc}`);
+
+  // Fetch food data from FDC (FoodData Central) API
+  let fdc_res = null;
+  try {
+    fdc_res = await fetch(
+      `https://api.nal.usda.gov/fdc/v1/foods/search?query=${upc}&pageSize=1&api_key=${process.env.FDC_API_KEY}`
+    );
+  } catch (err) {
+    console.error("Error fetching from FDC API: ");
+    console.error(err);
+    return Response.error();
+  }
+  // Parse JSON response from FDC API
+  const food_data = await fdc_res.json();
+
+  // Make sure that the food item was found
   if (food_data.foods[0]) {
     console.log(`Found ingredients: ${food_data.foods[0].ingredients}`);
 
@@ -130,7 +164,7 @@ express.get("/:upc", async (req, res) => {
     // Send response back to the client
     res.json(response);
   } else {
-    console.error(`No ingredients found for UPC ${upc}.`);
+    console.error(`No food item found for UPC ${upc}.`);
     return Response.error();
   }
 });
